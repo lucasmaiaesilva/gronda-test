03ce291b88f8808e1322a8c620fdfb4a2459bc74
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2021-08-08 15:54:01 -0300

    add bubbles on card layout

--- components/Card/styles.ts
+++ components/Card/styles.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 export const CardContainer = styled.div`
+  position: relative;
   padding: 2rem 2.2rem;
-  height: 8rem;
+  height: 12rem;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -10,6 +11,7 @@ export const CardContainer = styled.div`
   color: #fff;
   font-family: sans-serif;
   border-radius: .4rem;
+  overflow: hidden;
 
   & > * {
     padding: 0;
@@ -17,15 +19,38 @@ export const CardContainer = styled.div`
   }
 
   .type {
-    font-size: 1.4rem;
+    font-size: 1.6rem;
   }
 
   .value {
-    font-size: 2.2rem;
+    font-size: 2.8rem;
   }
 
   .period {
     font-size: 1.4rem;
+  }
+
+  &:before,
+  &:after {
+    content: "";
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
+    opacity: 0.5;
+  }
+
+  &:before {
+    width: 20rem;
+    height: 20rem;
+    right: -5rem;
+    bottom: -9rem;
+  }
+
+  &:after {
+    width: 13rem;
+    height: 13rem;
+    top: -4rem;
+    right: -6rem;
   }
 
   background-color: tomato;
